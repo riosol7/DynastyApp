82c0aa6f361616b33dba0969ca39d5f3c5996aff
Pirai Ojeda (Rio) <87847753+riosol7@users.noreply.github.com>
2022-05-01 16:43:15 -0700

    rosters updated

--- BE/controllers/player.js
+++ BE/controllers/player.js
@@ -14,44 +14,23 @@ playerController.get("/all" , async (req, res) => {
 
 playerController.get("/rosters", async (req, res) => {
     try {
-        // find rosters
         const rosters = req.body
-        // find all players
-        const foundPlayers = await Player.find()
-        // map foundPlayers by player_id
-        // Use For Each to iterate through the rosters, then map over the starters field and set the found player
-        
-        // function updateRoster(rosters, foundPlayers) {
-        //     return rosters.map(roster =>
-        //         roster.players.map(function (player) {
-        //             foundPlayers.find(foundPlayer => foundPlayer.player_id = player)
-                    
-        //             return player
-        //         })
-        //     )
-        // }
-
         let mappedRosters = rosters.map(async (roster) => {
-            const found = await Player.find({"player_id": roster.players})
-            console.log("found", found)
-            return found
+            const foundPlayers = await Player.find({"player_id": roster.players})
+            const foundStarters = await Player.find({"player_id": roster.starters})
+            const foundReserve = await Player.find({"player_id": roster.reserve})
+            const foundTaxi = await Player.find({"player_id": roster.taxi})
+            return {...roster, 
+                players:foundPlayers,
+                starters:foundStarters,
+                reserve:foundReserve,
+                taxi:foundTaxi
+            }
         })
-        
-        // let mappedRosters = rosters.map((roster) => {
-        //     try{
-        //         roster.players.map( async (player) => {
-        //             const foundPlayer = await Player.find({ "player_id": player})
-        //             console.log("foundPlayer", foundPlayer)
-        //             return foundPlayer
-
-        //         })
-        //     } catch (err) {
-        //         console.log(err)
-        //     }
-        // })
         const promise = await Promise.all(mappedRosters)
         console.log("mappedRosters", promise)
         res.status(200).json(promise)
+
     } catch (err) {
         res.status(400).json({
             error: err.message
