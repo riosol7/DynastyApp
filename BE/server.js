7a3e9868254abb8de276e8e0d061af0a0f8c7bca
Pirai Ojeda (Rio) <87847753+riosol7@users.noreply.github.com>
2022-04-30 17:58:29 -0700

    mongodb connected

---
+++ BE/server.js
@@ -0,0 +1,29 @@
+require("./db")
+require("dotenv").config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const methodOverride = require("method-override");
+
+const port = process.env.PORT || 5000;
+
+const whiteList = ["http://localhost:3000"];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whiteList.indexOf(origin) !== -1 || !origin || 1==1) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by Cors"));
+        }
+    },
+};
+
+app.use(methodOverride("_method"));
+app.use(cors(corsOptions));
+app.use(express.json());
+
+
+app.listen(port, () => {
+    console.log(`DynastyDB is running on port ${port}`)
+})
--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import './App.css';
+import React, { useEffect, useState } from "react"
 
 function App() {
+
+  const [data, setData] = useState([])
+
+  const getLeague = async () => {
+    try{
+      const league =await fetch("https://api.sleeper.app/v1/league/786065005090189312/rosters")
+      const parsedLeague = await league.json()
+      setData(parsedLeague)
+      console.log(data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  
+  useEffect(() => {
+    getLeague();
+    // eslint-disable-next-line 
+  }, [])
+
   return (
     <div className="App">
     </div>
