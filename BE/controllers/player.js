7cff1412183ac5b2fb40f75049949470d213dcef
Pirai Ojeda (Rio) <87847753+riosol7@users.noreply.github.com>
2022-05-03 14:25:11 -0700

    naming tweaks

--- BE/controllers/player.js
+++ BE/controllers/player.js
@@ -17,6 +17,7 @@ playerController.get("/rosters", async (req, res) => {
     try {
         const getRosters = await fetch("https://api.sleeper.app/v1/league/786065005090189312/rosters")
         const parsedRosters = await getRosters.json()
+        console.log("parsedRosters",parsedRosters)
 
         const getUsers = await fetch("https://api.sleeper.app/v1/league/786065005090189312/users")
         const parsedUsers = await getUsers.json()
@@ -24,16 +25,47 @@ playerController.get("/rosters", async (req, res) => {
         const getKCT = await fetch("https://sheetdb.io/api/v1/gultqvcl60sw9")
         const parsedKCT = await getKCT.json()
 
-        let rankings = parsedKCT.map(async kct => {
+        let mergeKCT = parsedKCT.map(async kct => {
             const foundPlayers = await Player.find({"full_name":kct.player, "position": kct.position})
             // console.log("foundPlayer",foundPlayers)
-            return {
-                ...kct,
-                foundPlayers
+            let sleeperID = 0 
+            for(let i=0; i < foundPlayers.length; i++){
+                // console.log("foundPlayer",foundPlayers[i].player_id)
+                sleeperID = foundPlayers[i].player_id
+            
+                return {
+                    ...kct,
+                    player_id: sleeperID
+                }
             }
+            // return {
+            //     ...kct,
+            //     player_id: foundPlayers
+            // }
         })
-        const testing = await Promise.all(rankings)
-        console.log("testing",testing)
+        const updatedPlayers = await Promise.all(mergeKCT)
+        // console.log("updatedPlayers:",updatedPlayers)
+
+        // let mapRosters = parsedRosters.map((roster) => {
+        //     let test = []
+        //     for (let i = 0; i < roster.length; i ++){
+        //         for(let j = 0; j < roster[i].players.length; j ++){
+        //             console.log("foundPlayers:", roster[i].players[j])
+        //             const foundPlayers = updatedPlayers.find(player => player.player_id === roster[i].players[j])
+        //             // console.log("foundPlayers:", foundPlayers)
+        //             //     const foundStarters = await Player.find({"player_id": roster.starters})
+        //             //     const foundReserve = await Player.find({"player_id": roster.reserve})
+        //             //     const foundTaxi = await Player.find({"player_id": roster.taxi})
+        //             test = foundPlayers
+        //             return test
+        //         }
+        //     return test
+        //     }
+        // })
+
+
+
+
 
         // let testRosters = parsedRosters.map(async (roster) => {
         //     const foundPlayers = await Player.find({"player_id": roster.players})
@@ -57,49 +89,66 @@ playerController.get("/rosters", async (req, res) => {
         // }
        
 
-        let mappedRosters = parsedRosters.map(async (roster) => {
-            const foundPlayers = await Player.find({"player_id": roster.players})
-            // console.log("foundPlayers:", foundPlayers)
-            const foundStarters = await Player.find({"player_id": roster.starters})
-            const foundReserve = await Player.find({"player_id": roster.reserve})
-            const foundTaxi = await Player.find({"player_id": roster.taxi})
+        // let mappedRosters = parsedRosters.map(async (roster) => {
+        //     const foundPlayers = await Player.find({"player_id": roster.players})
+        //     // console.log("foundPlayers:", foundPlayers)
+        //     const foundStarters = await Player.find({"player_id": roster.starters})
+        //     const foundReserve = await Player.find({"player_id": roster.reserve})
+        //     const foundTaxi = await Player.find({"player_id": roster.taxi})
 
-            let foundUser = parsedUsers.find(user => user.user_id === roster.owner_id)
+        //     let foundUser = parsedUsers.find(user => user.user_id === roster.owner_id)
            
-            let kctRankings = foundPlayers.map((player) => {
-                let kctPlayer = parsedKCT.find((KCTplayer) => KCTplayer.player === player.full_name)
-                let value = kctPlayer ? kctPlayer.rating : 0
-                // console.log("kctPlayer:",kctPlayer)
-                return { 
-                    ...player,
-                    kct: value
-                }
-            })
-            // console.log("kctRankings:", kctRankings)
+        //     for(let i = 0; i < foundPlayers.length; i ++){
+              
+        //         let kctPlayer = parsedKCT.find((KCTplayer) => KCTplayer.player === foundPlayers[i].full_name)
+        //             // console.log("kctPlayer",kctPlayer)
+        //         let value = kctPlayer ? kctPlayer.rating : 0
+    
+        //         return {...roster, 
+        //             owner_id:foundUser,
+        //             players:foundPlayers,
+        //             starters:foundStarters,
+        //             reserve:foundReserve,
+        //             taxi:foundTaxi
+        //         } 
+
+               
+        //     }
+
+        //     // let kctRankings = foundPlayers.map((player) => {
+        //     //     let kctPlayer = parsedKCT.find((KCTplayer) => KCTplayer.player === player.full_name)
+        //     //     let value = kctPlayer ? kctPlayer.rating : 0
+        //     //     // console.log("kctPlayer:",kctPlayer)
+        //     //     return { 
+        //     //         ...player,
+        //     //         kct: value
+        //     //     }
+        //     // })
+        //     // console.log("kctRankings:", kctRankings)
 
 
-            return {...roster, 
-                owner_id:foundUser,
-                players:kctRankings,
-                starters:foundStarters,
-                reserve:foundReserve,
-                taxi:foundTaxi
-            } 
-            // console.log("foundKCTPlayer:", foundKCTPlayer)
+        //     // return {...roster, 
+        //     //     owner_id:foundUser,
+        //     //     players:kctRankings,
+        //     //     starters:foundStarters,
+        //     //     reserve:foundReserve,
+        //     //     taxi:foundTaxi
+        //     // } 
+        //     // console.log("foundKCTPlayer:", foundKCTPlayer)
 
 
         
-            // let foundKCTPlayer = parsedKCT.find(player => player.Player === roster)
+        //     // let foundKCTPlayer = parsedKCT.find(player => player.Player === roster)
 
-            // return {...roster, 
-            //     owner_id:foundUser,
-            //     players:foundPlayers,
-            //     starters:foundStarters,
-            //     reserve:foundReserve,
-            //     taxi:foundTaxi
-            // }
-        })
-        const promise = await Promise.all(mappedRosters)
+        //     // return {...roster, 
+        //     //     owner_id:foundUser,
+        //     //     players:foundPlayers,
+        //     //     starters:foundStarters,
+        //     //     reserve:foundReserve,
+        //     //     taxi:foundTaxi
+        //     // }
+        // })
+        // const promise = await Promise.all(mappedRosters)
         // console.log("mappedRosters:", promise)
 
         // if(promise){
@@ -122,7 +171,7 @@ playerController.get("/rosters", async (req, res) => {
         //     // console.log("updatedRosters:", updatedRosters)
         //     res.status(200).json(updatedRosters)
         // } else {
-            res.status(200).json(testing)
+            res.status(200).json(updatedPlayers)
 
         // }
 
