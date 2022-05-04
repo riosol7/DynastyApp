a22d7eb67055b835c3cd22299e19e7a94f2eca97
Pirai Ojeda (Rio) <87847753+riosol7@users.noreply.github.com>
2022-05-03 22:05:56 -0700

    roster BUGsterminated

--- BE/controllers/player.js
+++ BE/controllers/player.js
@@ -26,7 +26,12 @@ playerController.get("/rosters", async (req, res) => {
         const parsedKCT = await getKCT.json()
 
         let mergeKCT = parsedKCT.map(async kct => {
-            const foundPlayers = await Player.find({"full_name":kct.player, "position": kct.position})
+            const foundPlayers = await Player.find(
+                {  
+                    "full_name": kct.player, 
+                    "position": kct.position
+                }
+            )
             let sleeperID = 0 
             for(let i=0; i < foundPlayers.length; i++){
                 sleeperID = foundPlayers[i].player_id
@@ -41,28 +46,65 @@ playerController.get("/rosters", async (req, res) => {
         const cleanDB = await KCT.deleteMany({})
         const insertKCT = await KCT.insertMany(updatedPlayers)
 
+        const filtered = {
+            age:1, 
+            college:1,
+            fantasy_data_id:1,
+            full_name:1,
+            height:1,
+            number:1,
+            player_id:1,
+            position:1,
+            team:1, 
+            weight:1,
+            years_exp:1
+        }
+
         let mappedRosters = parsedRosters.map(async (roster) => {
-            const foundPlayers = await Player.find({"player_id": roster.players})
+            const foundPlayers = await Player.find({"player_id": roster.players}, filtered)
             const kctRankings = await KCT.find({"player_id": roster.players})
             const foundStarters = await KCT.find({"player_id": roster.starters})
-            const foundReserve = await Player.find({"player_id": roster.reserve})
-            const foundTaxi = await Player.find({"player_id": roster.taxi})
+            const foundReserve = await Player.find({"player_id": roster.reserve}, filtered)
+            const foundTaxi = await Player.find({"player_id": roster.taxi}, filtered)
 
             let foundUser = parsedUsers.find(user => user.user_id === roster.owner_id)
+
+            let qbTotal = kctRankings.filter(player => player.position === "QB")
+            .map(player => player.rating)
+            .reduce((a, b) => Number(a) + Number(b), 0)
+
+            let rbTotal = kctRankings.filter(player => player.position === "RB")
+            .map(player => player.rating)
+            .reduce((a, b) => Number(a) + Number(b), 0)
+
+            let wrTotal = kctRankings.filter(player => player.position === "WR")
+            .map(player => player.rating)
+            .reduce((a, b) => Number(a) + Number(b), 0)
+
+            let teTotal = kctRankings.filter(player => player.position === "TE")
+            .map(player => player.rating)
+            .reduce((a, b) => Number(a) + Number(b), 0)
         
+            let teamTotal = qbTotal + rbTotal + wrTotal + teTotal
+
             return {...roster, 
                 owner_id:foundUser,
                 players:foundPlayers,
                 starters:foundStarters,
                 reserve:foundReserve,
                 taxi:foundTaxi,
-                kct:kctRankings
+                kct:kctRankings,
+                teamTotal:teamTotal,
+                qbTotal:qbTotal,
+                rbTotal:rbTotal,
+                wrTotal:wrTotal,
+                teTotal:teTotal
             } 
         })
         const promise = await Promise.all(mappedRosters)
 
         res.status(200).json(promise)
-
+        // console.log("promise:", promise)
     } catch (err) {
         res.status(400).json({
             error: err.message
