b572db6ca44555afa98667a03fa9ccdfcb4558b9
Pirai Ojeda (Rio) <87847753+riosol7@users.noreply.github.com>
2022-05-05 21:05:24 -0700

    layout init

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -86,90 +86,90 @@ export default function Home () {
         return rank
     }
 
-  return (
-    <div className="">
+    return (
+        <div className="">
         {
             isLoading ? <p>Loading </p> :
-            <div>
-                {
-                    rosters.teamRank.map((roster, idx) => 
-                        <div key={idx}>
-                            {
-                                roster.kct.owner.team_name ?
-                                <p>{roster.rank}
-                                {
-                                    roster.rank === 1? "st" : 
-                                    roster.rank === 2? "nd" : 
-                                    roster.rank === 3? "rd" : "th"
-                                }: {roster.kct.owner.team_name}
-                                </p>
-                                :
-                                <p>{roster.rank}
-                                {
-                                    roster.rank === 1?  "st" : 
-                                    roster.rank === 2? "nd" : 
-                                    roster.rank === 3? "rd" : "th"
-                                }: {roster.kct.owner.display_name}</p>
-                            }
-                            <p>Team total: {roster.kct.teamTotal}</p>
-                            <img alt="avatar" src={
-                                `https://sleepercdn.com/avatars/thumbs/${
-                                    roster.kct.owner.avatar
-                                }`
-                            }/>
-                            <p>QB rank: {qbRankings(roster)} - {roster.kct.qb.total}</p>
-                            {
-                                roster.kct.qb.players.map((player, i) =>
-                                <div key={i}>
-                                    <img alt="thumb" src={`https://sleepercdn.com/content/nfl/players/thumb/${player.player_id}.jpg`}/>
-                                    <p>{player.position} {player.player}</p>
-                                    <p>{player.team}</p>
-                                    <p>age: {player.age}</p>
-                                    <p>value: {player.rating}</p>
-                                </div>
-                                )
-                            }
-                            <p>RB rank: {rbRankings(roster)} - {roster.kct.rb.total}</p>
-                            {
-                                roster.kct.rb.players.map((player, i) =>
-                                <div key={i}>
-                                    <img alt="thumb" src={`https://sleepercdn.com/content/nfl/players/thumb/${player.player_id}.jpg`}/>
-                                    <p>{player.position} {player.player}</p>
-                                    <p>{player.team}</p>
-                                    <p>age: {player.age}</p>
-                                    <p>value: {player.rating}</p>
-                                </div>
-                                )
-                            }
-                            <p>WR rank: {wrRankings(roster)} - {roster.kct.wr.total}</p>
-                            {
-                                roster.kct.wr.players.map((player, i) =>
-                                <div key={i}>
-                                    <img alt="thumb" src={`https://sleepercdn.com/content/nfl/players/thumb/${player.player_id}.jpg`}/>
-                                    <p>{player.position} {player.player}</p>
-                                    <p>{player.team}</p>
-                                    <p>age: {player.age}</p>
-                                    <p>value: {player.rating}</p>
-                                </div>
-                                )
-                            }
-                             <p>TE rank: {teRankings(roster)} - {roster.kct.te.total}</p>
-                            {
-                                roster.kct.te.players.map((player, i) =>
-                                <div key={i}>
-                                    <img alt="thumb" src={`https://sleepercdn.com/content/nfl/players/thumb/${player.player_id}.jpg`}/>
-                                    <p>{player.position} {player.player}</p>
-                                    <p>{player.team}</p>
-                                    <p>age: {player.age}</p>
-                                    <p>value: {player.rating}</p>
-                                </div>
-                                )
-                            }
+            <div className="row">
+            {
+                rosters.teamRank.map((roster, idx) => 
+                    <div key={idx} className="col-md-4">
+                    {
+                        roster.kct.owner.team_name ?
+                        <p>{roster.rank}
+                        {
+                            roster.rank === 1? "st" : 
+                            roster.rank === 2? "nd" : 
+                            roster.rank === 3? "rd" : "th"
+                        }: {roster.kct.owner.team_name}
+                        </p>
+                        :
+                        <p>{roster.rank}
+                        {
+                            roster.rank === 1?  "st" : 
+                            roster.rank === 2? "nd" : 
+                            roster.rank === 3? "rd" : "th"
+                        }: {roster.kct.owner.display_name}</p>
+                    }
+                    <p>Team total: {roster.kct.teamTotal}</p>
+                    <img alt="avatar" src={
+                        `https://sleepercdn.com/avatars/thumbs/${
+                            roster.kct.owner.avatar
+                        }`
+                    }/>
+                    <p>QB rank: {qbRankings(roster)} - {roster.kct.qb.total}</p>
+                    {
+                        roster.kct.qb.players.map((player, i) =>
+                        <div key={i}>
+                            <img alt="thumb" src={`https://sleepercdn.com/content/nfl/players/thumb/${player.player_id}.jpg`}/>
+                            <p>{player.position} {player.player}</p>
+                            <p>{player.team}</p>
+                            <p>age: {player.age}</p>
+                            <p>value: {player.rating}</p>
                         </div>
-                    )
-                }
+                        )
+                    }
+                    <p>RB rank: {rbRankings(roster)} - {roster.kct.rb.total}</p>
+                    {
+                        roster.kct.rb.players.map((player, i) =>
+                        <div key={i}>
+                            <img alt="thumb" src={`https://sleepercdn.com/content/nfl/players/thumb/${player.player_id}.jpg`}/>
+                            <p>{player.position} {player.player}</p>
+                            <p>{player.team}</p>
+                            <p>age: {player.age}</p>
+                            <p>value: {player.rating}</p>
+                        </div>
+                        )
+                    }
+                    <p>WR rank: {wrRankings(roster)} - {roster.kct.wr.total}</p>
+                    {
+                        roster.kct.wr.players.map((player, i) =>
+                        <div key={i}>
+                            <img alt="thumb" src={`https://sleepercdn.com/content/nfl/players/thumb/${player.player_id}.jpg`}/>
+                            <p>{player.position} {player.player}</p>
+                            <p>{player.team}</p>
+                            <p>age: {player.age}</p>
+                            <p>value: {player.rating}</p>
+                        </div>
+                        )
+                    }
+                        <p>TE rank: {teRankings(roster)} - {roster.kct.te.total}</p>
+                    {
+                        roster.kct.te.players.map((player, i) =>
+                        <div key={i}>
+                            <img alt="thumb" src={`https://sleepercdn.com/content/nfl/players/thumb/${player.player_id}.jpg`}/>
+                            <p>{player.position} {player.player}</p>
+                            <p>{player.team}</p>
+                            <p>age: {player.age}</p>
+                            <p>value: {player.rating}</p>
+                        </div>
+                        )
+                    }
+                    </div>
+                )
+            }
             </div>
         }
-    </div>
-  );
+        </div>
+    );
 }
