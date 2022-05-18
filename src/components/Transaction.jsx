3016fc5d4c69310477b045765091e17d6b4a1264
Pirai Ojeda (Rio) <87847753+riosol7@users.noreply.github.com>
2022-05-17 20:34:23 -0700

    trades add section init

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -4,6 +4,22 @@ export default function Transaction(props) {
     const transactions = props.transactions
     const isLoading = props.isLoading
 
+    let findPlayer = (activity, players, playerID) => {
+        if(activity === "adds") {
+            let foundPlayerKCT = players.adds.playersKCT.filter(player => player.player_id === playerID)
+            let foundPlayer = players.adds.players.filter(player => player.player_id === playerID)
+            if(foundPlayerKCT === [] || null){
+                return foundPlayer[0]
+            } else
+            return foundPlayerKCT[0]
+        }
+    }
+
+    let findOwner = (ownerID, owners) => {
+        let foundOwner = owners.filter(owner => owner.roster_id === ownerID)
+        return foundOwner[0]
+    }
+
     return (
         <>
         {
@@ -11,20 +27,41 @@ export default function Transaction(props) {
                 transactions.map((transaction, i) => 
                     <div key={i}>
                         {
-                            // adds && drops
-                            transaction.adds !== null && transaction.drops !== null?
-                            <></>
+                            transaction.type === "trade" ?
+                            <>
+                            {
+                                //adds 
+                                Object.keys(transaction.adds).map((transactionID, i) => 
+                                    <div key={i}>
+                                        <p>Adds</p>
+                                        <p>player: {findPlayer("adds", transaction.playerDB, transactionID).player}</p>
+                                        <p>owner: {findOwner(transaction.adds[transactionID], transaction.roster_ids).display_name}</p>
+                                    </div>
+                                )
+                            }                               
+                            {/* {
+                                //drops
+                                Object.keys(transaction.drops).map(rosterID => rosterID)
+                            } */}
+                            </>
+                            :
+                            // adds && drops via waiver/commissioner/free agent
+                            transaction.adds !== null && transaction.drops !== null && transaction.type !== "trade"?
+                            <>
+                                {/* <p>test</p> */}
+                            </>
                             :
                             // only drops
                             transaction.adds === null ? 
-                            <></>
+                            <>
+                            
+                            </>
                             :
                             // only adds
                             transaction.drops === null ?
-                            <></>
-                            :
-                            transaction.type === "trade" ?
-                            <></>
+                            <>
+                            
+                            </>
                             :
                             <>
                             </>
