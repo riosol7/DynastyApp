94e36af6a443b40a9de1bd9ab2c67eac99305cb3
Pirai Ojeda (Rio) <87847753+riosol7@users.noreply.github.com>
2022-05-28 16:09:14 -0700

    32 team logos sorted

---
+++ src/assets/logo.js
@@ -0,0 +1,67 @@
+import ATL from "falcons.webp";
+import ARI from "cardinals.webp";
+import BAL from "ravens.webp";
+import BUF from "bills.webp";
+import CAR from "panthers.webp";
+import CHI from "bears.webp";
+import CIN from "bengals.webp";
+import CLE from "browns.webp";
+import DAL from "cowboys.webp";
+import DEN from "broncos.webp";
+import DET from "lions.webp";
+import GB from "packers.webp";
+import HOU from "texans.webp";
+import IND from "colts.webp";
+import JAX from "jaguars.webp";
+import KC from "chiefs.webp";
+import LAC from "chargers.webp";
+import LAR from "rams.webp";
+import LV from "raiders.webp";
+import MIA from "dolphins.webp";
+import MIN from "vikings.webp";
+import NE from "patriots.webp";
+import NO from "saints.webp";
+import NYG from "giants.webp";
+import NYJ from "jets.webp";
+import PHI from "eagles.webp";
+import PIT from "steelers.webp";
+import SEA from "seahawks.webp";
+import SF from "niners.webp";
+import TB from "bucs.webp";
+import TEN from "titans.webp";
+import WAS from "commanders.webp";
+
+const logos = {
+    ATL:ATL,
+    ARI:ARI,
+    BAL:BAL,
+    BUF:BUF,
+    CAR:CAR,
+    CHI:CHI,
+    CIN:CIN,
+    CLE:CLE,
+    DAL:DAL,
+    DEN:DEN,
+    DET:DET,
+    GB:GB,
+    HOU:HOU,
+    IND:IND,
+    JAX:JAX,
+    KC:KC,
+    LAC:LAC,
+    LAR:LAR,
+    LV:LV,
+    MIA:MIA,
+    MIN:MIN,
+    NE:NE,
+    NO:NO,
+    NYG:NYG,
+    NYJ:NYJ,
+    PHI:PHI,
+    PIT:PIT,
+    SEA:SEA,
+    SF:SF,
+    TB:TB,
+    TEN:TEN,
+    WAS:WAS
+}
